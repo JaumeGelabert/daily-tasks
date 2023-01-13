65109ed86ab84838289f460852926d2923042b4e
Falsitus <gelabertgalmes98@gmail.com>
2023-01-13 14:22:40 +0100

    More Vercel config (CORS)

--- client/src/components/utils/UrlBase.js
+++ client/src/components/utils/UrlBase.js
@@ -1,3 +1,7 @@
-const UrlBase = 'http://localhost:3001';
+// Desarrollo
+// const UrlBase = 'http://localhost:3001';
+
+// Producción
+const UrlBase = 'https://daily-tasks-api.vercel.app/';
 
 export default UrlBase;
--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://daily-tasks-jaume.vercel.app',
   })
 );
 
